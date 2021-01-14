cbf4ab7a1f1a42c4d3426b05327714fabd1a6b06
Tracie Tesoro <TGTESORO@sentara.com>
2021-01-14 14:00:53 -0500

    Add vars.tf

--- main.tf
+++ main.tf
@@ -1,28 +1,3 @@
-variable "name" {
-  type = string
-  description = "Application name"
-}
-
-variable "iduri" {
-  type = string
-  description = "This resources app id (TODO: figure out how to use self.id or use output of this to create identifier_uris)"
-}
-
-variable "owners" {
-  type = list(string)
-  description = "Application owners"
-}
-
-variable "reply_urls" {
-  type = list(string)
-  description = "Application allowed reply urls"
-}
-
-variable "logout_url" {
-  type = string
-  description = "Application reply urls"
-}
-
 terraform {
   required_version = ">= 0.12"
   required_providers {
